6dfd31dd6589efc3197620722396c73ac6cdc42c
luis carlos silva <luis.silva.702@ufrn.edu.br>
2022-12-02 12:28:49 -0300

    clear comments

--- main.py
+++ main.py
@@ -13,11 +13,3 @@ Group_3 = [pesos_list[6],pesos_list[7],pesos_list[8]]
 
 
 peso(Group_1,Group_2,Group_3)
-
-print('Lógica')
-print('1) Divida as bolas em 3 grupos;')
-print('2) Escolha dois grupos para pesar e reserve o grupo extra;')
-print('3) Coloque-os cada um em um lado da balança;')
-print('4) Se os pesos forem iguais, descarte ambos os grupos;')
-print('5) Senão, descarte o grupo mais pesado e o grupo extra;')
-print('6) A bola que restou é a mais pesada;')
